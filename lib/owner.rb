917fdd889f226ced6ea7118f3bcc8e4c84e76365
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 22:46:33 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,13 +33,15 @@ class Owner
     end
 
     def walk_dogs
-
       @pets[:dogs].each do |dog|
         dog.mood = "happy"
-
       end
+    end
 
-
+    def play_with_cats
+      @pets[:cats].each do |cat|
+        cat.mood = "happy"
+      end
     end
 
 
