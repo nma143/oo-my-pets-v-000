2100306409ec986cb19451b5080be53566d48198
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 22:53:48 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,7 +55,7 @@ class Owner
       @pets.each do |species, pets|
         pets.each do |pet|
           pet.mood = "nervous"
-          puts pet.mood
+          puts "#{pet.name} is #{pet.mood}"
         end
 
       end
