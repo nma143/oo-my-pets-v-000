24733270099d28fb9d064128c16b460a4d86f9b1
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 22:35:01 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,7 +2,8 @@ class Owner
   # code goes here
   @@all = []
 
-    attr_accessor :species, :pets
+    attr_accessor :pets
+    attr_reader : :species
 
     def initialize(species)
       @species = species
