9827a82603045c14da25f2fd48ed645a187989fb
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 22:47:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,6 +44,11 @@ class Owner
       end
     end
 
+    def feed_fish
+      @pets[:fish].each do |fish|
+        fish.mood = "happy"
+      end
+    end
 
     def say_species
       return "I am a #{@species}."
