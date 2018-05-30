bf35f570aaf4a5f11ad64a29bf9a7400abb00ed5
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 22:25:33 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
 class Cat
   # code goes here
+  attr_writer :name
+
+  def initialize(name)
+    @name = name
+  end
+
+
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -2,19 +2,19 @@ class Owner
   # code goes here
 
   attr_accessor :pets
-  
+
   def initialize
-    
+
     @pets = {
       fishes: [],
       cats: [],
       dogs: []}
-    } 
+    }
   end
 
   def buy_cat(name)
     new_pet = Cat.new(name)
   end
 
-  
+
 end
