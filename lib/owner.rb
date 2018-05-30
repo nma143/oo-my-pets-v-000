40a15dc585e21952bad240e3125a50ceb0089f66
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-30 22:25:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
   # code goes here
+
+  attr_accessor :pets
+  
+  def initialize
+    
+    @pets = {
+      fishes: [],
+      cats: [],
+      dogs: []}
+    } 
+  end
+
+  def buy_cat(name)
+    new_pet = Cat.new(name)
+  end
+
+  
 end
